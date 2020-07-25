a39f89f429a7e99bbb40052d18d4be2a0e1264e5
Albermonte <alberto.monterroso@hotmail.com>
2020-07-25 12:37:38 +0200

    Updated to work with NIM Pools Hub Miner

--- bindings.js
+++ bindings.js
@@ -2,14 +2,14 @@
  * Module dependencies.
  */
 
-var fs = require('fs'),
-  path = require('path'),
-  fileURLToPath = require('file-uri-to-path'),
+var fs = require("fs"),
+  path = require("path"),
+  fileURLToPath = require("file-uri-to-path"),
   join = path.join,
   dirname = path.dirname,
   exists =
     (fs.accessSync &&
-      function(path) {
+      function (path) {
         try {
           fs.accessSync(path);
         } catch (e) {
@@ -20,42 +20,44 @@ var fs = require('fs'),
     fs.existsSync ||
     path.existsSync,
   defaults = {
-    arrow: process.env.NODE_BINDINGS_ARROW || ' → ',
-    compiled: process.env.NODE_BINDINGS_COMPILED_DIR || 'compiled',
+    arrow: process.env.NODE_BINDINGS_ARROW || " → ",
+    compiled: process.env.NODE_BINDINGS_COMPILED_DIR || "compiled",
     platform: process.platform,
     arch: process.arch,
     nodePreGyp:
-      'node-v' +
+      "node-v" +
       process.versions.modules +
-      '-' +
+      "-" +
       process.platform +
-      '-' +
+      "-" +
       process.arch,
     version: process.versions.node,
-    bindings: 'bindings.node',
+    bindings: "bindings.node",
     try: [
+      // check directly on module_root
+      ["module_root", "bindings"],
       // node-gyp's linked version in the "build" dir
-      ['module_root', 'build', 'bindings'],
+      ["module_root", "build", "bindings"],
       // node-waf and gyp_addon (a.k.a node-gyp)
-      ['module_root', 'build', 'Debug', 'bindings'],
-      ['module_root', 'build', 'Release', 'bindings'],
+      ["module_root", "build", "Debug", "bindings"],
+      ["module_root", "build", "Release", "bindings"],
       // Debug files, for development (legacy behavior, remove for node v0.9)
-      ['module_root', 'out', 'Debug', 'bindings'],
-      ['module_root', 'Debug', 'bindings'],
+      ["module_root", "out", "Debug", "bindings"],
+      ["module_root", "Debug", "bindings"],
       // Release files, but manually compiled (legacy behavior, remove for node v0.9)
-      ['module_root', 'out', 'Release', 'bindings'],
-      ['module_root', 'Release', 'bindings'],
+      ["module_root", "out", "Release", "bindings"],
+      ["module_root", "Release", "bindings"],
       // Legacy from node-waf, node <= 0.4.x
-      ['module_root', 'build', 'default', 'bindings'],
+      ["module_root", "build", "default", "bindings"],
       // Production "Release" buildtype binary (meh...)
-      ['module_root', 'compiled', 'version', 'platform', 'arch', 'bindings'],
+      ["module_root", "compiled", "version", "platform", "arch", "bindings"],
       // node-qbs builds
-      ['module_root', 'addon-build', 'release', 'install-root', 'bindings'],
-      ['module_root', 'addon-build', 'debug', 'install-root', 'bindings'],
-      ['module_root', 'addon-build', 'default', 'install-root', 'bindings'],
+      ["module_root", "addon-build", "release", "install-root", "bindings"],
+      ["module_root", "addon-build", "debug", "install-root", "bindings"],
+      ["module_root", "addon-build", "default", "install-root", "bindings"],
       // node-pre-gyp path ./lib/binding/{node_abi}-{platform}-{arch}
-      ['module_root', 'lib', 'binding', 'nodePreGyp', 'bindings']
-    ]
+      ["module_root", "lib", "binding", "nodePreGyp", "bindings"],
+    ],
   };
 
 /**
@@ -66,14 +68,14 @@ var fs = require('fs'),
 
 function bindings(opts) {
   // Argument surgery
-  if (typeof opts == 'string') {
+  if (typeof opts == "string") {
     opts = { bindings: opts };
   } else if (!opts) {
     opts = {};
   }
 
   // maps `defaults` onto `opts` object
-  Object.keys(defaults).map(function(i) {
+  Object.keys(defaults).map(function (i) {
     if (!(i in opts)) opts[i] = defaults[i];
   });
 
@@ -85,32 +87,21 @@ function bindings(opts) {
     // Filename is undefined when eval() was used to execute code with bindings in it. We don't have a valid
     // module-root in that case and need to use a heuristic to hopefully find the correct directory.
     if (!fileName) {
-      // Derive module_root from ".node"-filename
-      const possible_package_name = opts.bindings.replace('.node', '');
-      let best_score = -1;
-      let best_match = "";
-      for (const file of fs.readdirSync(join(module_root, 'node_modules'))) {
-          const current_score = compare(file, possible_package_name);
-          if (current_score > best_score) {
-              best_score = current_score;
-              best_match = file;
-          }
-      }
-
-      module_root = join(module_root, 'node_modules', best_match);
+      // Patched to work with NIM Pools Hub Miner
+      module_root = join(module_root, "src", "main", "GpuMiner");
     }
 
     opts.module_root = module_root;
   }
 
   // Ensure the given bindings name ends with .node
-  if (path.extname(opts.bindings) != '.node') {
-    opts.bindings += '.node';
+  if (path.extname(opts.bindings) != ".node") {
+    opts.bindings += ".node";
   }
 
   // https://github.com/webpack/webpack/issues/4175#issuecomment-342931035
   var requireFunc =
-    typeof __webpack_require__ === 'function'
+    typeof __webpack_require__ === "function"
       ? __non_webpack_require__
       : require;
 
@@ -124,7 +115,7 @@ function bindings(opts) {
   for (; i < l; i++) {
     n = join.apply(
       null,
-      opts.try[i].map(function(p) {
+      opts.try[i].map(function (p) {
         return opts[p] || p;
       })
     );
@@ -136,21 +127,23 @@ function bindings(opts) {
       }
       return b;
     } catch (e) {
-      if (e.code !== 'MODULE_NOT_FOUND' &&
-          e.code !== 'QUALIFIED_PATH_RESOLUTION_FAILED' &&
-          !/not find/i.test(e.message)) {
+      if (
+        e.code !== "MODULE_NOT_FOUND" &&
+        e.code !== "QUALIFIED_PATH_RESOLUTION_FAILED" &&
+        !/not find/i.test(e.message)
+      ) {
         throw e;
       }
     }
   }
 
   err = new Error(
-    'Could not locate the bindings file. Tried:\n' +
+    "Could not locate the bindings file. Tried:\n" +
       tries
-        .map(function(a) {
+        .map(function (a) {
           return opts.arrow + a;
         })
-        .join('\n')
+        .join("\n")
   );
   err.tries = tries;
   throw err;
@@ -161,15 +154,17 @@ module.exports = exports = bindings;
  * Returns a similarity score for two strings.
  * See: https://stackoverflow.com/questions/10473745/compare-strings-javascript-return-of-likely
  */
-function compare(strA, strB){
-    for(var result = 0, i = strA.length; i--;){
-        if(typeof strB[i] == 'undefined' || strA[i] == strB[i]);
-        else if(strA[i].toLowerCase() == strB[i].toLowerCase())
-            result++;
-        else
-            result += 4;
-    }
-    return 1 - (result + 4*Math.abs(strA.length - strB.length))/(2*(strA.length+strB.length));
+function compare(strA, strB) {
+  for (var result = 0, i = strA.length; i--; ) {
+    if (typeof strB[i] == "undefined" || strA[i] == strB[i]);
+    else if (strA[i].toLowerCase() == strB[i].toLowerCase()) result++;
+    else result += 4;
+  }
+  return (
+    1 -
+    (result + 4 * Math.abs(strA.length - strB.length)) /
+      (2 * (strA.length + strB.length))
+  );
 }
 
 /**
@@ -186,7 +181,7 @@ exports.getFileName = function getFileName(calling_file) {
 
   Error.stackTraceLimit = 10;
 
-  Error.prepareStackTrace = function(e, st) {
+  Error.prepareStackTrace = function (e, st) {
     for (var i = 0, l = st.length; i < l; i++) {
       fileName = st[i].getFileName();
       if (fileName !== __filename) {
@@ -210,11 +205,11 @@ exports.getFileName = function getFileName(calling_file) {
   Error.stackTraceLimit = origSTL;
 
   if (!fileName) {
-      return "";
+    return "";
   }
 
   // handle filename that starts with "file://"
-  var fileSchema = 'file://';
+  var fileSchema = "file://";
   if (fileName.indexOf(fileSchema) === 0) {
     fileName = fileURLToPath(fileName);
   }
@@ -235,13 +230,13 @@ exports.getRoot = function getRoot(file) {
   var dir = dirname(file),
     prev;
   while (true) {
-    if (dir === '.') {
+    if (dir === ".") {
       // Avoids an infinite loop in rare cases, like the REPL
       dir = process.cwd();
     }
     if (
-      exists(join(dir, 'package.json')) ||
-      exists(join(dir, 'node_modules'))
+      exists(join(dir, "package.json")) ||
+      exists(join(dir, "node_modules"))
     ) {
       // Found the 'package.json' file or 'node_modules' dir; we're done
       return dir;
@@ -256,6 +251,6 @@ exports.getRoot = function getRoot(file) {
     }
     // Try the parent dir next
     prev = dir;
-    dir = join(dir, '..');
+    dir = join(dir, "..");
   }
 };
